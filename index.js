1dadd7da543990e28619c3919b1aa64371669d7a
LeightonSkervin <lskervin23@gmail.com>
2024-01-30 13:09:56 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+const random = 0 + 1
+const num3 = 14
+const num4 = 10
+const mod = num3%num4
+const max = 20
